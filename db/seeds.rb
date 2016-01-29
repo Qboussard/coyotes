60be0f4ea42ea5552182c31b8ac8bf57c8ca8419
Quentin Boussard <quentin.bouss@gmail.com>
2016-01-29 11:21:33 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(email: 'test@test.fr', password: '123456', password_confirmation: "123456", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-02-06 14:02:10", last_sign_in_at: "2015-02-06 14:02:10", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1")
+		puts '-> Ajoût de l\'utilisateur test@test.fr avec le mot de passe suivant : 123456'
