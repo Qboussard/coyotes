0448d50c7d2495af9bc5f35cbb5f0e823321ac4e
Quentin Boussard <quentin.bouss@gmail.com>
2016-02-04 14:47:31 +0100

    add 3 tournament in layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,11 @@
 class ArticlesController < ApplicationController
     def index
         	@news = News.order('id desc')
+            @tournaments = Tournament.order('id desc')
         end
 
         def show
         	@new = News.find(params[:id])
+            @tournaments = Tournament.order('id desc')
         end
 end
