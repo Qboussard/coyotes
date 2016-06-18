f78c76f50b0e684fc418557bf1cc49b7821beed9
Cle61 <prodhomme.clement@hotmail.fr>
2016-06-18 20:16:26 +0200

    compilation 2

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Coyotes
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.serve_static_assets = true
   end
 end
