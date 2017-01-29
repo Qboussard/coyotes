5391cbd7d8e4af14498906b866134db380ea3765
Cle61 <prodhomme.clement@hotmail.fr>
2017-01-29 22:04:50 +0100

    translate calendar

--- app/assets/javascripts/admin/event.js
+++ app/assets/javascripts/admin/event.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 	$('.datepicker').pickadate({
 			selectMonths: true, // Creates a dropdown to control month
-			selectYears: 15 // Creates a dropdown of 15 years to control year
+			selectYears: 15, // Creates a dropdown of 15 years to control year
+      monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+      weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+      today: 'aujourd\'hui',
+      clear: 'effacer',
+      formatSubmit: 'dd/mm/yyyy'
 		});
 });
