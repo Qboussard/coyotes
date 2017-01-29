af4cf161973b4353478dbd3c2348f447f18b69d1
Quentin Boussard <quentin.bouss@gmail.com>
2017-01-29 21:21:40 +0100

    debug db events

--- db/migrate/20170115151051_rename_calendars_to_events.rb
+++
@@ -1,5 +0,0 @@
-class RenameEventsToEvents < ActiveRecord::Migration
-  def change
-		rename_table :events, :events
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115151051) do
+ActiveRecord::Schema.define(version: 20161229090723) do
 
   create_table "events", force: :cascade do |t|
     t.date     "date"
