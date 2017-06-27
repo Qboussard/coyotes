d9ebe549829619fb000896b28333d15ae60da5d2
Quentin Boussard <quentin.bouss@gmail.com>
2017-06-28 01:19:17 +0200

    change color charts and add javascript action

--- app/assets/javascripts/admin/dashboard.js
+++ app/assets/javascripts/admin/dashboard.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$( document ).ready(function() {
+	console.log('ready');
+	$(".top-nav").on("swipe",function(){
+		console.log('hide');
+		$(this).hide();
+	});
+});
