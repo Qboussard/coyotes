ea66b0c6521aaed91bf215c4edc20a68c1cc841e
Quentin Boussard <quentin.bouss@gmail.com>
2017-06-30 16:53:18 +0200

    can edit a admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,9 +4,28 @@ class Admin::UsersController < Admin::DashboardController
     @users = User.order('id desc')
   end
 
+	def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(new_params)
+      # Handle a successful update.
+      redirect_to admin_users_path, notice: 'le compte a bien été modifié'
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     User.destroy(params[:id])
     redirect_to admin_users_path, notice: 'Utilisateur a bien été supprimé'
+  end
+
+	def new_params
+    params.require(:user).permit(:admin)
   end
 
 end
