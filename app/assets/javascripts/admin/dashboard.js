b9a013f30a9692f065f5844f2df6572aee403953
Quentin Boussard <quentin.bouss@gmail.com>
2017-07-21 13:11:19 +0200

    test dropdown option admin on articles

--- app/assets/javascripts/admin/dashboard.js
+++ app/assets/javascripts/admin/dashboard.js
@@ -1,9 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $( document ).ready(function() {
-	console.log('ready');
-	$(".top-nav").on("swipe",function(){
-		console.log('hide');
-		$(this).hide();
-	});
+	$('.dropdown-button').dropdown({
+	      inDuration: 300,
+	      outDuration: 225,
+	      constrainWidth: false, // Does not change width of dropdown to that of the activator
+	      hover: false, // Activate on hover
+	      gutter: 0, // Spacing from edge
+	      belowOrigin: false, // Displays dropdown below the button
+	      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+	      stopPropagation: false // Stops event propagation
+	    }
+	  );
 });
